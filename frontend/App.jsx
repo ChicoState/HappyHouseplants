d29842028f40b05b7e77a7f5865b95daf6fe4e03
Dylan Davidson <turtwigandmew@yahoo.com>
2020-09-28 16:26:12 -0700

    Added rudimentary naviagation system and calendar

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, View, Text} from 'react-native';
 
-export default function App() {
+
+import Calend from './components/Calendar';
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Calend></Calend>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            otherParam: 'List of all of your plants',
+          });
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text></Text>
+      <Text>{JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
---
+++ frontend/components/Calendar.jsx
@@ -0,0 +1,80 @@
+import {Calendar} from 'react-native-calendars';
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default class Calend extends React.Component {
+  render() {
+    return (
+      <View>
+        <Calendar
+          style={{borderWidth: 5, borderColor: 'green', height: 400}}
+          theme={{
+            backgroundColor: '#ffffff',
+            calendarBackground: '#ffffff',
+            textSectionTitleColor: 'black',
+            textSectionTitleDisabledColor: '#d9e1e8',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#00adf5',
+            dayTextColor: '#2d4150',
+            textDisabledColor: 'red',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'green',
+            disabledArrowColor: '#d9e1e8',
+            monthTextColor: 'green',
+            indicatorColor: 'blue',
+            textDayFontFamily: 'monospace',
+            textMonthFontFamily: 'monospace',
+            textDayHeaderFontFamily: 'monospace',
+            textDayFontWeight: '300',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '300',
+            textDayFontSize: 20,
+            textMonthFontSize: 28,
+            textDayHeaderFontSize: 17,
+          }}
+          // Collection of dates that have to be marked. Default = {}
+          markedDates={{
+            '2020-09-25': {
+              selected: true,
+              marked: true,
+              selectedColor: 'green',
+            },
+            '2020-09-29': {marked: true},
+            '2020-09-30': {marked: true, dotColor: 'red', activeOpacity: 0},
+            '2020-09-28': {disabled: true, disableTouchEvent: true},
+          }}
+          // Initially visible month. Default = Date()
+          current={'2020-09-23'}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={'2020-01-01'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2020-12-30'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {
+            console.log('selected day', day);
+          }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {
+            console.log('month changed', month);
+          }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Enable the option to swipe between months. Default = false
+          enableSwipeMonths={true}
+        />
+        <Text>Green = Seed Ready</Text>
+      </View>
+    );
+  }
+}
