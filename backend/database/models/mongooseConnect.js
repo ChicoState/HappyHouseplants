c37899b6c38c4117f681b8ad46cbcd75debb3345
Neodusk <jetou12@ymail.com>
2020-09-30 15:51:24 -0700

    Fixed linting issues

--- backend/database/models/mongooseConnect.js
+++ backend/database/models/mongooseConnect.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectionURL = `${process.env.CONNECT_URL}${process.env.DB_NAME}`
+const connectionURL = `${process.env.CONNECT_URL}${process.env.DB_NAME}`;
 
 mongoose.connect(connectionURL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-  }).catch(error => console.log(`Error:${error}`));
+    useFindAndModify: false,
+  }).catch((error) => console.log(`Error:${error}`));
 
-mongoose.connection.on('connecting', ()=> {});
-mongoose.connection.on('connected', ()=> {})
-mongoose.connection.on('disconnecting', ()=> {})
-mongoose.connection.on('disconnected', ()=> {})
-mongoose.connection.on('reconnectFailed', ()=> {})
+mongoose.connection.on('connecting', () => { console.log('Connecting'); });
+mongoose.connection.on('connected', () => { console.log('Connected'); });
+mongoose.connection.on('disconnecting', () => { console.log('Disconnecting'); });
+mongoose.connection.on('disconnected', () => { console.log('Disconnected'); });
+mongoose.connection.on('reconnectFailed', () => { console.log('Reconnect failed'); });
 
 module.exports.databaseConnection = mongoose.connection;
