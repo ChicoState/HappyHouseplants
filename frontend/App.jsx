07b703623d5e5465c13ea403e36aed701f012875
Dylan Davidson <turtwigandmew@yahoo.com>
2020-10-05 15:44:03 -0700

    Fixed linting issues with App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,50 +1,46 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Button, View, Text } from 'react-native';
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import Calend from './components/Calendar';
 
 const Stack = createStackNavigator();
 
-function HomeScreen({ navigation }) {
+function HomeScreen(obj) {
+  const { navigation } = obj;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title='Go to Library' onPress={() => { navigation.navigate('Library', { otherParam: 'List of all of your plants' }); }} />
-      <Text> </Text>
-      <Button title='Go to Calendar' onPress={() => { navigation.navigate('Calendar', { otherParam: 'Calendar for tracking seed growth' }); }} />
+      <Calend />
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            otherParam: 'List of all of your plants',
+          });
+        }}
+      />
     </View>
   );
 }
 
-function LibraryScreen({ route, navigation }) {
+function DetailsScreen(obj) {
   /* 2. Get the param */
-  const { itemId } = route.params;
+  const { route, navigation } = obj;
   const { otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Library Screen</Text>
+      <Text>Details Screen</Text>
+      <Text />
       <Text>{JSON.stringify(otherParam)}</Text>
-      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
-      <Text></Text>
-      <Button title='Go back' onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
-
-function CalendarScreen({ route, navigation }) {
-  /* 2. Get the param */
-  const { itemId } = route.params; 
-  const { otherParam } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Calendar Screen</Text>
-      <Text>{JSON.stringify(otherParam)}</Text>
-      <Calend></Calend>
-      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
-      <Text></Text>
-      <Button title='Go back' onPress={() => navigation.goBack()} />
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details', {})}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -52,15 +48,12 @@ function CalendarScreen({ route, navigation }) {
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Library' component={LibraryScreen} />
-        <Stack.Screen name='Calendar' component={CalendarScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-
