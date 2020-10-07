8fcba963772bb7ff77ced290c496480513f2e1ac
Neodusk <jetou12@ymail.com>
2020-10-06 18:32:32 -0700

    Update calendar to update date color on touch

--- frontend/components/Calendar.jsx
+++ frontend/components/Calendar.jsx
@@ -1,13 +1,26 @@
-import {Calendar} from 'react-native-calendars';
-import {View, Text} from 'react-native';
+/* eslint-disable react/prefer-stateless-function */
+import { Calendar } from 'react-native-calendars';
+import { View, Text } from 'react-native';
 import React from 'react';
 
+const datesMarked = {
+/*   '2020-09-25': {
+    selected: true,
+    marked: true,
+    selectedColor: 'green',
+  },
+  '2020-09-29': {marked: true},
+  '2020-09-30': {marked: true, dotColor: 'red', activeOpacity: 0},
+  '2020-09-28': {disabled: true, disableTouchEvent: true}, */
+};
+const firstDayOfYear = new Date(new Date().getFullYear(), 0, 1);
+const lastDayOfYear = new Date(new Date().getFullYear(), 11, 31);
 export default class Calend extends React.Component {
   render() {
     return (
       <View>
         <Calendar
-          style={{borderWidth: 5, borderColor: 'green', height: 400}}
+          style={{ borderWidth: 5, borderColor: 'green', height: 400 }}
           theme={{
             backgroundColor: '#ffffff',
             calendarBackground: '#ffffff',
@@ -36,27 +49,34 @@ export default class Calend extends React.Component {
           }}
           // Collection of dates that have to be marked. Default = {}
           markedDates={{
-            '2020-09-25': {
-              selected: true,
-              marked: true,
-              selectedColor: 'green',
-            },
-            '2020-09-29': {marked: true},
-            '2020-09-30': {marked: true, dotColor: 'red', activeOpacity: 0},
-            '2020-09-28': {disabled: true, disableTouchEvent: true},
+            ...datesMarked,
           }}
           // Initially visible month. Default = Date()
-          current={'2020-09-23'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2020-01-01'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2020-12-30'}
+          // current={'2020-09-23'}
+          // Minimum date that can be selected,
+          // dates before minDate will be grayed out. Default = undefined
+          minDate={firstDayOfYear}
+          // Maximum date that can be selected,
+          // dates after maxDate will be grayed out. Default = undefined
+          maxDate={lastDayOfYear}
           // Handler which gets executed on day press. Default = undefined
           onDayPress={(day) => {
             console.log('selected day', day);
+            console.log(day.dateString);
+            if (datesMarked[day.dateString]) {
+              delete datesMarked[day.dateString];
+            } else {
+              datesMarked[day.dateString] = {
+                selected: true,
+                marked: true,
+                selectedColor: 'green',
+              };
+            }
+            console.log(datesMarked);
+            this.setState(datesMarked);
           }}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'MMMM yyyy'}
+          monthFormat="MMMM yyyy"
           // Handler which gets executed when visible month changes in calendar. Default = undefined
           onMonthChange={(month) => {
             console.log('month changed', month);
@@ -65,10 +85,12 @@ export default class Calend extends React.Component {
           hideArrows={false}
           // Do not show days of other months in month page. Default = false
           hideExtraDays={true}
-          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+          // If hideArrows=false and hideExtraDays=false
+          // do not swich month when tapping on greyed out
           // day from another month that is visible in calendar page. Default = false
           disableMonthChange={true}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          /* If firstDay=1 week starts from Monday. 
+          Note that dayNames and dayNamesShort should still start from Sunday. */
           firstDay={1}
           // Enable the option to swipe between months. Default = false
           enableSwipeMonths={true}
