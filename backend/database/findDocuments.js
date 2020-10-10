d2a59a7a24bd1895023961fe7586a1910674e98c
Cessna210 <aleggett@mail.csuchico.edu>
2020-10-09 19:31:56 -0700

    Fixed a lint error in findDocuments.js

--- backend/database/findDocuments.js
+++ backend/database/findDocuments.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function findDocuments(collectionName, query) {
-  const promise = new Promise(resolve => {
+  const promise = new Promise((resolve) => {
     const collection = mongoose.connection.db.collection(collectionName);
     collection.find(query).toArray((err, docs) => {
       resolve(docs);
