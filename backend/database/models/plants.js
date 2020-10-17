9051eec95ccc1f4f84d92ac70313d83fcee9d668
Cessna210 <aleggett@mail.csuchico.edu>
2020-10-16 21:07:41 -0700

    Fixed a tiny typo in plants.js schema comment.

--- backend/database/models/plants.js
+++ backend/database/models/plants.js
@@ -26,7 +26,7 @@ const plantSchema = new mongoose.Schema({
     comment: String,
     temperature: {
       min: Number, // Celcius
-      max: Number, // Celcuis
+      max: Number, // Celcius
     },
   },
   toxicity: {
