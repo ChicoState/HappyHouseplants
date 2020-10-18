61d72fa29fb779492152c30da9c57ba2a28aefb7
iramosromero <ramos.israel916@gmail.com>
2020-10-17 17:48:10 -0700

    fixed linting errors inside Search.jsx and changed button label

--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, Dimensions, StyleSheet, LayoutAnimation} from 'react-native';
-import { ScrollView } from 'react-native';
-import { Image } from 'react-native';
-import { Input, Button, Card, Layout } from '@ui-kitten/components';
-import { SERVER_ADDR } from '../server'
+import {
+  ScrollView, Image, Text, StyleSheet,
+} from 'react-native';
+import {
+  Input, Button, Card, Layout,
+} from '@ui-kitten/components';
 import PropTypes from 'prop-types';
-
-const styles = require('./SearchBarStyles');
+import * as eva from '@eva-design/eva';
+import { SERVER_ADDR } from '../server';
 
 class SearchBar extends Component {
   constructor() {
@@ -19,33 +20,28 @@ class SearchBar extends Component {
       Alldata: [],
     };
 
-    this.styles = StyleSheet.create({ searchResultsContainer: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1
-    },
-    titleText: {
-      fontWeight: "bold"
-    },
-    card: {
-      marginVertical: 10,
-      maxWidth: 100,
-      flex: 1,
-      flexDirection: 'row',
-    },
-    container: {
-      paddingHorizontal: 70,
-    paddingVertical: 80,
-    },
-  })
+    this.styles = StyleSheet.create({
+      searchResultsContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+      },
+      titleText: {
+        fontWeight: 'bold',
+      },
+      card: {
+        marginVertical: 10,
+        maxWidth: 100,
+        flex: 1,
+        flexDirection: 'row',
+      },
+      container: {
+        paddingHorizontal: 70, paddingVertical: 80,
+      },
+    });
     this.searchPress = this.searchPress.bind(this);
-
-
-
   }
 
-  
-  
   componentDidMount() {
     const listThis = this;
     fetch(`${SERVER_ADDR}/plants/`)
@@ -61,80 +57,79 @@ class SearchBar extends Component {
         console.log(`Failed to load plant search. Reason: ${error}`);
         listThis.setState({ visible: false, error });
       });
-
   }
 
   searchPress() {
-      this.setState({
-        Results: this.state.Alldata.filter((x) => x.plantName.toLowerCase().includes(this.state.searchString.toLowerCase())),
-      });
+    const { searchString, Alldata } = this.state;
+    this.setState({
+      Results:
+      Alldata.filter(
+        (x) => x.plantName.toLowerCase().includes(searchString.toLowerCase()),
+      ),
+    });
+  }
+
+  render() {
+    const { loaded, error } = this.state;
+    const { onPressItem } = this.props;
+    if (error) {
+      const errMsg = `Failed to load search.\n${error}`;
+      return (<Text>{errMsg}</Text>);
     }
 
-
-    render() {
-      const { loaded, error, Alldata } = this.state;
-      const { onPressItem } = this.props;
-      if (error) {
-        const errMsg = `Failed to load search.\n${error}`;
-        return (<Text>{errMsg}</Text>);
-      }
-  
-      if (!loaded) {
-        return (<Text>Loading search...</Text>);
-      }
-      const renderItemHeader = (headerProps, info) => (
-        <Layout {...headerProps}>
-          <Text category="h6">
-            {info}
-          </Text>
-        </Layout>
-      );
-
-      const renderItemFooter = (footerProps) => (
-        <Text {...footerProps}>
-          Add
+    if (!loaded) {
+      return (<Text>Loading search...</Text>);
+    }
+    const renderItemHeader = (headerProps, info) => (
+      <Layout mapping={eva.mapping} theme={eva.light}>
+        <Text category="h6">
+          {info}
         </Text>
-      );
+      </Layout>
+    );
 
-      const myCards = this.state.Results.map((plant) => (
-      <Card 
-      key = {plant.plantID} 
-      style = {this.styles.searchResultsContainer}
-      status="basic"
-      header={(headerProps) => renderItemHeader(headerProps, plant.plantName)}
-      footer={renderItemFooter}
-      onPress={() => { onPressItem(plant); }}
+    const renderItemFooter = () => (
+      <Text mapping={eva.mapping} theme={eva.light}>
+        Add
+      </Text>
+    );
+    const { Results } = this.state;
+    const id = '_id';
+    const myCards = Results.map((plant) => (
+      <Card
+        key={plant[id]}
+        style={this.styles.searchResultsContainer}
+        status="basic"
+        header={(headerProps) => renderItemHeader(headerProps, plant.plantName)}
+        footer={renderItemFooter}
+        onPress={() => { onPressItem(plant); }}
       >
         <Image
           source={{ uri: plant.image.sourceURL }}
           style={{ width: 150, height: 150 }}
         />
       </Card>
-      ));
-        return(
+    ));
+    return (
 
-          <Layout>
-            <Layout>
-              <Input
-                placeholder = 'Enter your search terms'
-              
-                onChangeText = {(text) => this.setState({searchString: text}, () => console.log(this.state.searchString))}
-              />
-              <Button
-                
-                onPress = {() => this.searchPress()}
-              >
-              BUTTON
-              </Button>
-            </Layout>
-            <Layout style={{ flex: 1 }}>
-              <ScrollView style={{ flex: 1 }}>
-                {myCards}
-              </ScrollView>
-            </Layout>
-          </Layout>
-        )
-    }
+      <Layout>
+        <Layout>
+          <Input
+            placeholder="Enter your search terms"
+            onChangeText={(text) => this.setState({ searchString: text })}
+          />
+          <Button onPress={() => this.searchPress()}>
+            Search
+          </Button>
+        </Layout>
+        <Layout style={{ flex: 1 }}>
+          <ScrollView style={{ flex: 1 }}>
+            {myCards}
+          </ScrollView>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 SearchBar.propTypes = {
