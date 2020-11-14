d3c78a2fc8711050003794bb4ca6bead9fe3d000
Cessna210 <aleggett@mail.csuchico.edu>
2020-11-13 22:07:56 -0800

    Changed server.js to specifically send JSON responses, and added null checks to prevent server crashes

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get('/random_tips/', (req, res) => {
 app.get('/tips/', (req, res) => {
   const tipsQuery = {};
   findDocuments('tips', tipsQuery).then((docs) => {
-    res.send(docs);
+    res.json(docs);
   });
 });
 
@@ -53,9 +53,9 @@ app.get('/tips/:tipID', (req, res) => {
   const tipQuery = { tipID: req.params.tipID };
   findDocuments('tips', tipQuery).then((docs) => {
     if (docs.length < 1) {
-      res.status(404).send('Tip not found');
+      res.status(404).json({});
     } else {
-      res.send(docs[0]);
+      res.json(docs[0]);
     }
   });
 });
@@ -64,7 +64,7 @@ app.get('/users/:userId', (req, res) => {
   const userQuery = { userId: req.params.userId };
   findDocuments('Users', userQuery).then((docs) => {
     if (docs.length < 1) {
-      res.status(404).send('User not found');
+      res.status(404).json({});
     } else {
       // Don't send the whole 'user' document, that'll contain sensitive information!
       const user = docs[0];
@@ -74,52 +74,165 @@ app.get('/users/:userId', (req, res) => {
         lastName: user.lastName,
         username: user.username,
       };
-      res.send(publicUserInfo);
+      res.json(publicUserInfo);
     }
   });
 });
 
 app.get('/mytips', (req, res) => {
   authenticateUserRequest(req, res).then((userId) => {
-    findOneDocument('Users', { userId }).then((user) => {
-      res.send(user.savedTipsByID);
-    });
+    if (userId) {
+      findOneDocument('Users', { userId }).then((user) => {
+        res.json(user.savedTipsByID);
+      });
+    } else {
+      res.status(403).json({});
+    }
   });
 });
 
 app.get('/myplants', (req, res) => {
   authenticateUserRequest(req, res).then((userId) => {
-    findOneDocument('Users', { userId }).then((userDoc) => {
-      let myPlantsByID = [];
-      if (userDoc.myPlantsByID) {
-        myPlantsByID = userDoc.myPlantsByID;
-      }
+    if (userId) {
+      findOneDocument('Users', { userId }).then((userDoc) => {
+        let myPlantsByID = [];
+        if (userDoc.myPlantsByID) {
+          myPlantsByID = userDoc.myPlantsByID;
+        }
 
-      res.send(myPlantsByID);
-    });
+        res.json(myPlantsByID);
+      });
+    } else {
+      res.status(403).json({});
+    }
+  });
+});
+
+app.get('/savedplants', (req, res) => {
+  authenticateUserRequest(req, res).then((userId) => {
+    if (userId) {
+      findOneDocument('Users', { userId }).then((userDoc) => {
+        let savedPlantsByID = [];
+        if (userDoc.savedPlantsByID) {
+          savedPlantsByID = userDoc.savedPlantsByID;
+        }
+
+        res.json(savedPlantsByID);
+      });
+    } else {
+      res.status(403).json({});
+    }
   });
 });
 
 app.post('/myplants', (req, res) => {
   authenticateUserRequest(req, res).then((userId) => {
-    const query = { userId };
-    const plant = req.body;
-    findOneDocument('Users', query).then((userDoc) => {
-      let myPlantsByID = [];
-      if (userDoc.myPlantsByID) {
-        myPlantsByID = userDoc.myPlantsByID;
-      }
+    if (userId) {
+      const query = { userId };
+      const plant = req.body;
+      findOneDocument('Users', query).then((userDoc) => {
+        let myPlantsByID = [];
+        if (userDoc.myPlantsByID) {
+          myPlantsByID = userDoc.myPlantsByID;
+        }
 
-      myPlantsByID.push(plant);
+        myPlantsByID.push(plant);
 
-      USERS.updateOne(query, { myPlantsByID }).then(() => {
-        console.log(`Added plant ${plant} to user ID ${userId}'s plants.`);
-        res.status(201).send();
-      }).catch((saveError) => {
-        console.error(`Failed to add a plant to 'myplants' for user ID ${userId}. Reason: ${saveError}`);
-        res.status(500).send();
+        USERS.updateOne(query, { myPlantsByID }).then(() => {
+          console.log(`Added plant ${JSON.stringify(plant)} to user ID ${userId}'s owned plants.`);
+          res.status(201).json({});
+        }).catch((saveError) => {
+          console.error(`Failed to add a plant to 'myplants' for user ID ${userId}. Reason: ${saveError}`);
+          res.status(500).json({});
+        });
       });
-    });
+    } else {
+      res.status(403).json({});
+    }
+  });
+});
+
+app.post('/savedplants', (req, res) => {
+  authenticateUserRequest(req, res).then((userId) => {
+    if (userId) {
+      const query = { userId };
+      const plantID = req.body;
+      findOneDocument('Users', query).then((userDoc) => {
+        let savedPlantsByID = [];
+        if (userDoc.savedPlantsByID) {
+          savedPlantsByID = userDoc.savedPlantsByID;
+        }
+
+        savedPlantsByID.push(plantID);
+
+        USERS.updateOne(query, { savedPlantsByID }).then(() => {
+          console.log(`Added plant ID ${plantID} to user ID ${userId}'s saved plants.`);
+          res.status(201).json({});
+        }).catch((saveError) => {
+          console.error(`Failed to add a plant to 'savedplants' for user ID ${userId}. Reason: ${saveError}`);
+          res.status(500).json({});
+        });
+      });
+    } else {
+      res.status(403).json({});
+    }
+  });
+});
+
+app.delete('/myplants', (req, res) => {
+  authenticateUserRequest(req, res).then((userId) => {
+    if (userId) {
+      const query = { userId };
+      const plant = req.body;
+      findOneDocument('Users', query).then((userDoc) => {
+        let myPlantsByID = [];
+        if (userDoc.myPlantsByID) {
+          myPlantsByID = userDoc.myPlantsByID;
+        }
+
+        const index = myPlantsByID.indexOf(plant);
+        if (index > -1) {
+          myPlantsByID.splice(index, 1);
+        }
+
+        USERS.updateOne(query, { myPlantsByID }).then(() => {
+          console.log(`Removed plant ${JSON.stringify(plant)} from user ID ${userId}'s owned plants.`);
+          res.status(201).json({});
+        }).catch((saveError) => {
+          console.error(`Failed to remove a plant from 'myplants' for user ID ${userId}. Reason: ${saveError}`);
+          res.status(500).json({});
+        });
+      });
+    } else {
+      res.status(403).json({});
+    }
+  });
+});
+
+app.delete('/savedplants', (req, res) => {
+  authenticateUserRequest(req, res).then((userId) => {
+    if (userId) {
+      const query = { userId };
+      const { plantID } = req.body;
+      findOneDocument('Users', query).then((userDoc) => {
+        let savedPlantsByID = [];
+        if (userDoc.savedPlantsByID) {
+          savedPlantsByID = userDoc.savedPlantsByID;
+        }
+
+        const newSavedPlantsByID = savedPlantsByID.filter((plant) => plant.plantID !== plantID);
+
+        USERS.updateOne(query, { savedPlantsByID: newSavedPlantsByID }).then(() => {
+          console.log(`Removed plant ${plantID} from user ID ${userId}'s saved plants.`);
+          res.status(201).json({});
+        }).catch((saveError) => {
+          console.error(`Failed to remove a plant from 'savedplants' for user ID ${userId}. Reason: ${saveError}`);
+          res.status(500).json({});
+        });
+      });
+    } else {
+      res.status(403).json({});
+    }
   });
 });
 
@@ -129,45 +242,49 @@ app.get('/mycalendar/notes', (req, res) => {
       const query = { userId };
       findOneDocument('Users', query).then((userDoc) => {
         const { calendarNotes = {/* Default no notes */} } = userDoc;
-        res.send(calendarNotes);
+        res.json(calendarNotes);
       });
     } else {
-      res.status(403).send();
+      res.status(403).json({});
     }
   });
 });
 
 app.post('/mycalendar/notes/', (req, res) => {
   authenticateUserRequest(req, res).then((userId) => {
-    const query = { userId };
-    const keys = Object.keys(req.body);
-    const values = Object.values(req.body);
-    const when = keys[0];
-    const note = values[0];
-    findOneDocument('Users', query).then((userDoc) => {
-      const { calendarNotes = {/* Default no notes */} } = userDoc;
+    if (userId) {
+      const query = { userId };
+      const keys = Object.keys(req.body);
+      const values = Object.values(req.body);
+      const when = keys[0];
+      const note = values[0];
+      findOneDocument('Users', query).then((userDoc) => {
+        const { calendarNotes = {/* Default no notes */} } = userDoc;
 
-      if (calendarNotes[when]) {
-        // Append to the existing date's notes
-        calendarNotes[when].push(note);
-      } else {
-        // Create a new date:note pair
-        calendarNotes[when] = [note];
-      }
-      USERS.updateOne(query, { calendarNotes }).then(() => {
-        res.status(201).send();
-      }).catch((saveError) => {
-        console.error(`Failed to post a note (${JSON.stringify(note)}) to the calendar for user ID ${userId}. Reason: ${saveError}`);
-        res.status(500).send();
+        if (calendarNotes[when]) {
+          // Append to the existing date's notes
+          calendarNotes[when].push(note);
+        } else {
+          // Create a new date:note pair
+          calendarNotes[when] = [note];
+        }
+        USERS.updateOne(query, { calendarNotes }).then(() => {
+          res.status(201).json({});
+        }).catch((saveError) => {
+          console.error(`Failed to post a note (${JSON.stringify(note)}) to the calendar for user ID ${userId}. Reason: ${saveError}`);
+          res.status(500).json({});
+        });
       });
-    });
+    } else {
+      res.status(403).json({});
+    }
   });
 });
 
 app.get('/plants/', (req, res) => {
   const plantsQuery = {};
   findDocuments('Plants', plantsQuery).then((docs) => {
-    res.send(docs);
+    res.json(docs);
   });
 });
 
@@ -175,9 +292,9 @@ app.get('/plants/:plantID', (req, res) => {
   const userQuery = { plantID: req.params.plantID };
   findDocuments('Plants', userQuery).then((docs) => {
     if (docs.length < 1) {
-      res.status(404).send('Plant not found');
+      res.status(404).json({});
     } else {
-      res.send(docs[0]);
+      res.json(docs[0]);
     }
   });
 });
@@ -189,7 +306,8 @@ app.post('/register/', (req, res) => {
   register(username, password, firstName, lastName).then((status) => {
     res.json(status);
   }).catch((reason) => {
-    res.send(`Failed for reason: ${reason}`);
+    console.error(`Failed to register a user due to an error: ${reason}`);
+    res.json({});
   });
 });
 
@@ -198,7 +316,8 @@ app.post('/login/', (req, res) => {
   login(username, password).then((status) => {
     res.json(status);
   }).catch((reason) => {
-    res.send(`Failed for reason: ${reason}`);
+    console.error(`Failed to login a user due to an error: ${reason}`);
+    res.json({});
   });
 });
 
