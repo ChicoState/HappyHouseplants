537a362f8f22c694081a1960b235e85508fd1d79
Cessna210 <aleggett@mail.csuchico.edu>
2020-11-25 15:56:54 -0800

    Fixed a bug when submitting calendar notes

--- backend/routes/mycalendar.js
+++ backend/routes/mycalendar.js
@@ -15,31 +15,31 @@ module.exports = (app) => {
   }, true);
 
   authPost(app, '/mycalendar/notes/', (req, res, userDoc) => {
-    let calendarNotes = {};
     const keys = Object.keys(req.body);
     const values = Object.values(req.body);
     const when = keys[0];
     const note = values[0];
 
+    let calendarNotes = {};
     if (userDoc.calendarNotes) {
       calendarNotes = userDoc.calendarNotes;
-
-      if (calendarNotes[when]) {
-      // Append to the existing date's notes
-        calendarNotes[when].push(note);
-      } else {
-        // Create a new date:note pair
-        calendarNotes[when] = [note];
-      }
-
-      updateUserDocument(userDoc.userId, { calendarNotes })
-        .then(() => {
-          res.status(201).json({});
-        })
-        .catch((error) => {
-          console.error(`Failed to save a note due to an error: ${error}`);
-          res.status(500).json({});
-        });
     }
+
+    if (calendarNotes[when]) {
+    // Append to the existing date's notes
+      calendarNotes[when].push(note);
+    } else {
+      // Create a new date:note pair
+      calendarNotes[when] = [note];
+    }
+
+    updateUserDocument(userDoc.userId, { calendarNotes })
+      .then(() => {
+        res.status(201).json({});
+      })
+      .catch((error) => {
+        console.error(`Failed to save a note due to an error: ${error}`);
+        res.status(500).json({});
+      });
   }, true);
 };
