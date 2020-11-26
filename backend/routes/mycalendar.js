850e346059280f2c28cf853c9227f95afe22a296
Cessna210 <aleggett@mail.csuchico.edu>
2020-11-25 18:40:53 -0800

    Created calendar API

--- backend/routes/mycalendar.js
+++ backend/routes/mycalendar.js
@@ -1,12 +1,13 @@
 const {
   authGet,
   authPost,
+  authDelete,
   updateUserDocument,
 } = require('../api/auth');
 
 module.exports = (app) => {
   authGet(app, '/mycalendar/notes', (req, res, userDoc) => {
-    let calendarNotes = [];
+    let calendarNotes = {};
     if (userDoc.calendarNotes) {
       calendarNotes = userDoc.calendarNotes;
     }
@@ -26,7 +27,7 @@ module.exports = (app) => {
     }
 
     if (calendarNotes[when]) {
-    // Append to the existing date's notes
+      // Append to the existing date's notes
       calendarNotes[when].push(note);
     } else {
       // Create a new date:note pair
@@ -41,5 +42,51 @@ module.exports = (app) => {
         console.error(`Failed to save a note due to an error: ${error}`);
         res.status(500).json({});
       });
+  }, true);
+
+  authDelete(app, '/mycalendar/notes', (req, res, userDoc) => {
+    const keys = Object.keys(req.body);
+    const values = Object.values(req.body);
+    const when = keys[0];
+    const noteObj = values[0];
+
+    let calendarNotes = {};
+    if (userDoc.calendarNotes) {
+      calendarNotes = userDoc.calendarNotes;
+    }
+
+    let removed;
+    if (calendarNotes[when]) {
+      // Remove from the existing date's notes
+      const removeIndex = calendarNotes[when].findIndex(
+        (cur) => cur.note === noteObj.note && cur.dots === noteObj.dots,
+      );
+      if (removeIndex > -1) {
+        calendarNotes[when].splice(removeIndex, 1);
+
+        if (calendarNotes[when].length === 0) {
+          delete calendarNotes[when];
+        }
+
+        removed = true;
+      } else {
+        removed = false;
+      }
+    } else {
+      removed = false;
+    }
+
+    if (removed) {
+      updateUserDocument(userDoc.userId, { calendarNotes })
+        .then(() => {
+          res.json(true);
+        })
+        .catch((error) => {
+          console.error(`Failed to remove a calendar note for user ${userDoc.userId} due to an error: ${error}`);
+          res.status(500).json({});
+        });
+    } else {
+      res.json(false);
+    }
   }, true);
 };
