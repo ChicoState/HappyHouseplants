c87098dfceccb8df6c7a8b3cfec84b475d83745c
Neodusk <htortorich@mail.csuchico.edu>
2020-12-05 19:55:45 -0800

    Delete server.js

--- frontend/server.js
+++
@@ -1,7 +0,0 @@
-const PORT = '8080';
-if (!process || !process.env || !process.env.EXPO_HOSTNAME) {
-  process.env.EXPO_HOSTNAME = 'localhost';
-}
-const SERVER_ADDR = `http://10.0.0.228:${PORT}`;
-
-module.exports = { SERVER_ADDR };
