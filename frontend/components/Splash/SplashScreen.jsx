724fc91301c569ae76301e8051154d23ea449b26
Neodusk <jetou12@ymail.com>
2020-12-08 18:32:12 -0800

    Reset splashscreen

--- frontend/components/Splash/SplashScreen.jsx
+++ frontend/components/Splash/SplashScreen.jsx
@@ -1,10 +1,7 @@
 /* eslint-disable react/forbid-prop-types */
-/* eslint-disable global-require */
 import React from 'react';
-import { Alert, Image } from 'react-native';
-import {
-  Layout, Spinner, Text, Button,
-} from '@ui-kitten/components';
+import { View, Alert } from 'react-native';
+import { Spinner, Text, Button } from '@ui-kitten/components';
 import { PropTypes } from 'prop-types';
 
 const { autoLogin } = require('../Profile/auth-react');
@@ -65,17 +62,11 @@ class SplashScreen extends React.Component {
     const { loadingLoginInfo, loginRequired } = this.state;
     if (loginRequired && !loadingLoginInfo) {
       return (
-        <Layout style={{ flex: 1, alignItems: 'center' }}>
-          <Text />
-          <Text>Welcome! Please login or create an account.</Text>
-          <Image source={require('./fr.png')} />
-          <Text />
-          <Layout style={{ flexDirection: 'row' }}>
-            <Button onPress={this.loginRequested}> Login  </Button>
-            <Text> </Text>
-            <Button onPress={this.registerRequested}>Register</Button>
-          </Layout>
-        </Layout>
+        <View>
+          <Text>Welcome, please login or create an account.</Text>
+          <Button onPress={this.loginRequested}>Login</Button>
+          <Button onPress={this.registerRequested}>Create account</Button>
+        </View>
       );
     }
 
